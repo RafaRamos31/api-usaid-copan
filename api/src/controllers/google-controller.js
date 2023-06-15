6d188939609a363c926455c3c3b8bd1a79c67ad2
RafaRamos31 <rafaelramos19972@gmail.com>
2023-06-15 10:16:16 -0600

    correccion de nombre de credenciales

--- api/src/controllers/google-controller.js
+++ api/src/controllers/google-controller.js
@@ -6,7 +6,7 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const KEYFILENAME = path.join(__dirname, '../../../credenciales.json');
+const KEYFILENAME = path.join(__dirname, '../../../credentials.json');
 const SCOPES = ["https://www.googleapis.com/auth/drive"];
 
 const auth = new google.auth.GoogleAuth({
