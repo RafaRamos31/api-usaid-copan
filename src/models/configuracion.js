e35879fad448ffb635b76a1045d94104a17c6d23
RafaRamos31 <rafaelramos19972@gmail.com>
2023-12-06 21:19:29 -0600

    update Config Model

--- src/models/configuracion.js
+++ src/models/configuracion.js
@@ -1,10 +1,71 @@
 import mongoose from "mongoose";
 
+const valor = new mongoose.Schema({
+  nombre: {
+    type: String,
+  },
+  descripcion: {
+    type: String,
+  },
+})
+
+const red = new mongoose.Schema({
+  name: {
+    type: String,
+  },
+  exists: {
+    type: Boolean,
+  },
+  enlace: {
+    type: 'string',
+  },
+})
+
+
 const schema = new mongoose.Schema({
   ref: {
-    type: 'string',
+    type: String,
     required: true,
-  }
+  },
+  nosotros: {
+    type: String,
+  },
+  mensaje: {
+    type: String,
+  },
+  autor: {
+    type: String,
+  },
+  mision: {
+    type: String,
+  },
+  vision: {
+    type: String,
+  },
+  urlMapa: {
+    type: String,
+  },
+  valores: [{
+    type: valor,
+  }],
+  footerCorreo: {
+    type: String,
+  },
+  footerDireccion: {
+    type: String,
+  },
+  footerTelefonos: {
+    type: String,
+  },
+  footerDesc: {
+    type: String,
+  },
+  footerEnlace: {
+    type: String,
+  },
+  footerRedes: [{
+    type: red,
+  }],
 });
 
 /**
